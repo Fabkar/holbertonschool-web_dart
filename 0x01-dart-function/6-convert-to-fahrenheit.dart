5b73ddae0670fc49bd882455347e772c3881a6be
FabianCV <FabianCarmona@comfama.com.co>
2022-11-30 20:04:27 -0500

    dart function

---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,6 @@
+List<String> convertToF(List<double> tempInC) {
+    var idx = 0;
+    List<String> arr = [];
+    for (; idx < tempInC.length; idx++) { arr.add((tempInC[idx] * 9 / 5 + 32).toStringAsFixed(2)); }
+    return arr;
+}
